7b46b7d0c713f8bb880e44c779c388d803e3e45c
mugeshsamrajr <mugeshsamraj@gmail.com>
2025-12-27 06:41:36 +0530

    mailer

---
+++ Utils/mailer.js
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  // Gmail or yahoo or outlook
+  service: "Gmail",
+  auth: {
+    user: process.env.PASS_MAIL,
+    pass: process.env.PASS_KEY,
+  },
+});
+
+const sendEmail = async (to, subject, text) => {
+  const mailOptions = {
+    from: process.env.PASS_MAIL,
+    to,
+    subject,
+    text,
+  }
+  return transporter.sendMail(mailOptions);
+}
+
+export default sendEmail;
